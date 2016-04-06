fa7d3fc0b3298198994ca7265e44e1771760399b
andribja <andribja@gmail.com>
2016-04-06 17:01:05 +0000

    Added server model for messages and reviews along with geolocational search for ads

---
+++ config/message.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose'); 
+var message = require('./models').messages;  
+
+exports.sendMessage = function(subject, content, author_id, recipient_id, callback) {
+
+    var date = new Date().getTime();
+    var _id = new mongoose.Types.ObjectId;
+
+    var newMessage = new message({    
+            subject: subject,
+            content : content,
+            recipient_id : recipient_id,
+            author_id : author_id,
+            timePosted : date,
+            _id : _id
+        });
+
+    newMessage.save(function (err) {
+        callback({
+            'response': "Message sent",
+            'success': true,
+            '_id': _id
+        });
+    });
+}
+
+exports.getMessage = function(q, callback) {
+    var options = {
+        "limit":10,
+        "sort":"timePosted"
+    };
+
+    ad.find(q, callback);
+}
---
+++ config/review.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose'); 
+var review = require('./models').reviews;  
+
+exports.postReview = function(stars, content, author_id, reviewee_id, callback) {
+
+    var date = new Date().getTime();
+    var _id = new mongoose.Types.ObjectId;
+
+    var newReview = new review({    
+            stars : stars,
+            content : content,
+            author_id : author_id,
+            reviewee_id : reviewee_id,
+            timePosted : date,
+            _id : _id
+        });
+
+    newReview.save(function (err) {
+        callback({
+            'response': "Message sent",
+            'success': true,
+            '_id': _id
+        });
+    });
+}
+
+exports.getReview = function(q, callback) {
+    var options = {
+        "limit":10,
+        "sort":"timePosted"
+    };
+
+    ad.find(q, callback);
+}
